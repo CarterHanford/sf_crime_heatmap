fd2df4217ea35dd82462fea8f27725190d675720
Carter Hanford <carter.hanford@slu.edu>
2021-02-10 11:48:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
-        });
+// generate map and set location
+var map = L.map('map').setView([37.7, -122.4], 11);
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 4,
-          center: [-90, 38] // longitude, latitude
-        });
-      });
+// set map style
+var Stamen_Terrain = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+}).addTo(map);
+
+// add crime geojson and set to heatmap
+$.getJSON('https://raw.githubusercontent.com/gbrunner/adv-python-for-gis-and-rs/master/Week%201/sf_crime.geojson', function(data) {
+  var coordinatesOnly = data.features.map(function(feature) {
+    return [feature.geometry.coordinates[1], feature.geometry.coordinates[0], 1];
+  });
+
+  var heat = L.heatLayer(coordinatesOnly).addTo(map);
+});
